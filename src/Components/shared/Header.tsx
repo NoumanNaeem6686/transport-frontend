a239c37b37c5db6663d0732c0c0e69bb2613bdc7
Saif Ur Rehman <saifarshad3344@gmail.com>
2024-11-21 23:30:31 +0500

    fix : header

--- src/Components/shared/Header.tsx
+++ src/Components/shared/Header.tsx
@@ -1,33 +1,20 @@
-"use client"
-
-import { Menu } from "lucide-react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const Header = () => {
     const pathname = usePathname();
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [prevScrollPos, setPrevScrollPos] = useState(0);
     const [visible, setVisible] = useState(false);
 
-
-
     const navData = [
-        { name: "How It Works", path: "/how-works" },
-        { name: "Shipping Rates", path: "/shipping-rates" },
-        { name: "About", path: "/about" },
-        { name: "Contact Us", path: "/contact-us" },
-        { name: "Get Offer", path: "/get-offer", isButton: true },
+        { name: "Get Offer", path: "/get-offer", isButton: true, color: "white" },
+        { name: "Book Vehicle", path: "/book-vehicle", isButton: true, color: "black" },
     ];
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-
-    const handleLinkClick = () => {
-        toggleMenu();
-    };
+    const getOppositeColor = (color: any) => (color === "white" ? "black" : "white");
 
     useEffect(() => {
         let navbar = true;
@@ -45,15 +32,13 @@ const Header = () => {
         };
     }, []);
 
-
-
     return (
-        <div className={"flex w-full items-center justify-center"}>
+        <div className="flex w-full items-center justify-center">
             <nav
-                className={`w-full fixed h-20 flex flex-row items-center justify-between px-6 md:px-12 lg:px-10 xl:px-24 top-0 z-50
-            ${visible
-                        ? "bg-[#edf6f9]/85 backdrop:filter backdrop-blur-lg shadow-xl text-black z-40"
-                        : " bg-transparent text-black"
+                className={`w-full fixed h-20 flex flex-row items-center justify-between px-6 md:px-12 lg:px-10 top-0 z-50
+                ${visible
+                        ? "bg-[#949696]/85 backdrop:filter backdrop-blur-lg shadow-xl text-black z-40"
+                        : "bg-transparent text-black"
                     } transform transition-transform duration-300`}
             >
                 <Link href="/">
@@ -65,79 +50,24 @@ const Header = () => {
                     </h1>
                 </Link>
 
-                <div className="flex justify-end lg:hidden" onClick={toggleMenu}>
-                    <Menu
-                        className={` ${visible ? "text-black" : "wtext-hite"}  md:h-10 h-7 w-auto`}
-                    />
-                </div>
-
-                <div className={`hidden lg:flex flex-row gap-x-6`}>
-                    {navData.map((item, index) => (
-                        <Link key={index} href={item.path}>
-                            <div
-                                className={`py-1.5 ${visible || item.isButton ? "text-black" : "text-white"
-                                    }   text-base cursor-pointer hover:opacity-80
-                    ${item.isButton
-                                        ? "bg-white text-black border rounded-lg border-white px-5 py-1 hover:bg-black hover:text-white hover:border-black hover:opacity-100 transition-colors "
-                                        : ""
-                                    }
-                    ${pathname === item.path && !item.isButton
-                                        ? "opacity-75"
-                                        : ""
-                                    }`}
-                                onClick={handleLinkClick}
-                            >
-                                {item.name}
-                            </div>
-                        </Link>
-                    ))}
-
-                </div>
-
-                <div
-                    className={`fixed top-0 min-h-screen left-0 w-full bg-white py-4 shadow z-50 transform border-2 border-pink 
-                    transition-transform ease-in-out duration-300 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"
-                        } lg:hidden`}
-                >
-                    <div className="flex flex-col space-y-6 bg-white">
-                        <div className="flex justify-end pr-6">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                onClick={toggleMenu}
-                                className="w-6 h-6 text-black cursor-pointer"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
+                <div className="flex flex-row gap-x-6">
+                    <Link href={"/get-offer"}>
+                        <div
+                            className={`bg-white hover:bg-black hover:text-white py-2 px-5 min-w-[120px] rounded-lg cursor-pointer flex items-center justify-center text-base hover:border-black transition-colors`}
+                        >
+                            Get Offer
                         </div>
-                        {navData.map((item, index) => (
-                            <Link key={index} href={item.path}>
-                                <div
-                                    className={`px-3 py-1.5 text-xl mx-4 text-black cursor-pointer
-                      ${item.isButton
-                                            ? "bg-black text-white border rounded-lg border-white py-2.5 w-fit px-6"
-                                            : ""
-                                        }
-                      ${pathname.startsWith(item.path)
-                                            ? "font-medium text-black"
-                                            : ""
-                                        }`}
-                                    onClick={handleLinkClick}
-                                >
-                                    {item.name}
-                                </div>
-                            </Link>
-                        ))}
+                    </Link>
+                    <Link href={"/book-vehicle"}>
+                        <div
+                            className={`bg-black text-white hover:bg-white hover:text-black py-2 px-5 min-w-[120px] rounded-lg cursor-pointer flex items-center justify-center text-base hover:border-white transition-colors`}
 
-                    </div>
+                        >
+                            Book Vehicle
+                        </div>
+                    </Link>
                 </div>
+
             </nav>
         </div>
     );
